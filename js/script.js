66630508d1081e29e07e35df86318f87df3c94af
burillu <burillu90@hotmail.it>
2023-10-27 16:33:53 +0200

    add title and listener

--- js/script.js
+++ js/script.js
@@ -5,7 +5,17 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: `This is the first title print with Vue js...`,
+        toCenter:'text-center',
+        vueImgBack : 'https://crisp.chat/static/blog/content/images/2022/05/How-to-Migrate-a-large-project-from-Vue-2-to-Vue-3.jpg'
+
       }
-    }
+      
+    },
+    methods: {
+        printMsg: function(){
+            this.message=` And this is the second`
+            
+        }
+      }
   }).mount('#app')
