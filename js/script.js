201364a32e24f03493b613591f4776c6ffccc049
burillu <burillu90@hotmail.it>
2023-10-27 17:28:56 +0200

    add back ground, img and styles for second title

--- js/script.js
+++ js/script.js
@@ -2,20 +2,28 @@
 
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        message: `This is the first title print with Vue js...`,
-        toCenter:'text-center',
-        vueImgBack : 'https://crisp.chat/static/blog/content/images/2022/05/How-to-Migrate-a-large-project-from-Vue-2-to-Vue-3.jpg'
+        return {
+            message: `This is the first title print with Vue js...`,
+            classH1: 'text-center',
+            classImgVue: 'my-vue-img',
+            vueImg: 'url(https://pespantelis.github.io/vue-videobg/demo/assets/logo.png)',
+            divBg: 'my-vue-bg'
 
-      }
-      
+        }
+
     },
     methods: {
-        printMsg: function(){
-            this.message=` And this is the second`
-            
+        printMsg: function (e) {
+
+
+
+            this.message = ` And this is the second`;
+            this.vueImg = 'url(https://crisp.chat/static/blog/content/images/2022/05/How-to-Migrate-a-large-project-from-Vue-2-to-Vue-3.jpg)';
+            this.classH1 = 'text-white';
+            this.divBg += ' d-flex align-items-center';
+
         }
-      }
-  }).mount('#app')
+    }
+}).mount('#app')
